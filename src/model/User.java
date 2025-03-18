ba7e11f8a25bc4f6612628c184eb31538cd41657
Anthony Martin <anthonym7@arizona.edu>
2025-03-17 18:10:04 -0700

    Got rid of some redundent info that I didnt need in there

--- src/model/User.java
+++ src/model/User.java
@@ -1,17 +1,13 @@
 package model;
 
-import java.util.List;
-import java.util.ArrayList;
 
 public class User {
     private String username;
     private String password;
-    private List<Song> library;
 
     public User(String username, String password){
         this.username = username;
         this.password = password;
-        this.library = new ArrayList<>();
     }
 
     public String getUser(){
@@ -20,12 +16,5 @@ public class User {
 
     public String getPassword(){
         return password;
-    }
-     public List<Song> getLibrary(){
-        return library;
-    }
-
-    public void addSong(Song song){
-        library.add(song);
     }
 }
