20102f0a471fc8cd89479734f5376d1c3bd0f609
Anthony Martin <anthonym7@arizona.edu>
2025-03-20 12:11:26 -0700

    Updated for database stuff

--- src/model/Playlist.java
+++ src/model/Playlist.java
@@ -11,7 +11,7 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
-public class Playlist {
+public class Playlist{
     private String name;
     private List<Song> songs;
 
