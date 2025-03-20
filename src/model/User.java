3d88d6ce6eb1c1472826c4992c0d1b20e7b25fde
Anthony Martin <anthonym7@arizona.edu>
2025-03-20 12:10:50 -0700

    Added User class that handels user instances

--- src/model/User.java
+++ src/model/User.java
@@ -1,20 +1,42 @@
 package model;
-
+import store.MusicStore;
+import java.util.*;
 
 public class User {
     private String username;
-    private String password;
+    private LibraryModel library;
 
-    public User(String username, String password){
+    public User(String username) {
         this.username = username;
-        this.password = password;
+        this.library = new LibraryModel();
     }
 
-    public String getUser(){
+    public String getUsername() {
         return username;
     }
 
-    public String getPassword(){
-        return password;
+    public LibraryModel getLibrary() {
+        return library;
+    }
+
+    // User-level operations (delegating to LibraryModel)
+    public boolean addSong(Song song, MusicStore store) {
+        return library.addSong(song, store);
+    }
+
+    public boolean addAlbum(Album album, MusicStore store) {
+        return library.addAlbum(album, store);
+    }
+
+    public boolean createPlaylist(String name) {
+        return library.createPlaylist(name);
+    }
+
+    public Set<Song> listSongs() {
+        return library.getSong();
+    }
+
+    public Set<Album> listAlbums() {
+        return library.getAlbums();
     }
 }
